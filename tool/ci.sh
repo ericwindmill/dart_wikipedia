49f9d90b83d6deaf38c5dccdc6b9c650350ed02c
Eric Windmill <eric@ericwindmill.com>
2025-03-05 12:22:01 -0500

    complete

---
+++ tool/ci.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+flutter doctor -v
+
+declare -ar PROJECTS=(
+  "pkgs/cli"
+  "pkgs/command_runner"
+  "pkgs/flutter"
+  "pkgs/server"
+  "pkgs/wikipedia"
+)
+
+function ci_projects () {
+  shift
+  local arr=("$@")
+  for PROJECT in "${arr[@]}"
+  do
+    echo "=== Testing ${PROJECT} ==="
+    pushd "${PROJECT}"
+
+    flutter pub get
+    dart analyze --fatal-infos --fatal-warnings
+    dart format --output none .
+
+    if [ -d "test" ]
+    then
+      if grep -q "flutter:" "pubspec.yaml"; then
+        flutter test
+      else
+        dart test
+      fi
+    fi
+
+    popd
+  done
+}
+
+ci_projects "${PROJECTS[@]}"
+
+echo "=== Success ==="
